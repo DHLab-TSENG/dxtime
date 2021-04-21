06f8de2841a15addf494a577df8e190476dad9de
Annie0412 <55923137+Annie0412@users.noreply.github.com>
2021-04-22 01:28:12 +0800

    Add files via upload

---
+++ R/analyWindow_LSTM.R
@@ -0,0 +1,157 @@
+analyWindow_LSTM <- function(DataFile_cutdata,window_N,ID,periodage=NULL,DataFile_personal,label,predictor=c(),N,batch_size=100,Epoch=10,units=16){
+
+  #input cutdata
+  DataFile_cutdata <- as.data.table(DataFile_cutdata)
+  ccscategory <- c(ccsDxICD9$CCS_CATEGORY %>% unique())
+  if(is.null(periodage)){
+    dataCol <- c(deparse(substitute(window_N)),deparse(substitute(ID)),ccscategory)
+  }else{
+    dataCol <- c(deparse(substitute(window_N)),deparse(substitute(ID)),deparse(substitute(periodage)),ccscategory)
+  }
+  cutdata <- DataFile_cutdata[, dataCol,with=FALSE]
+  #input personal
+  DataFile_personal <- as.data.table(DataFile_personal)
+  dataCol <- c(deparse(substitute(ID)),deparse(substitute(label)),predictor)
+  personal <- DataFile_personal[, dataCol,with=FALSE]
+  personal[,c("SEX"):=lapply(.SD,function(x)ifelse(x=="F",0L,1L)),.SDcols=c("SEX")]
+  #將個人資料以及ccs的window資料合併成最後要用的資料表
+  maintable <- merge(cutdata,personal[,c("ID",predictor),with=FALSE],by="ID",all.x=T)
+  if(is.null(periodage)){
+    maintable <- cbind(maintable[,c("window_N","ID","SEX")],maintable[,-c("window_N","ID","SEX")])
+  }else{
+    maintable <- cbind(maintable[,c("window_N","ID","SEX","periodage")],maintable[,-c("window_N","ID","SEX","periodage")])
+  }
+  CCSWide_window <- list()
+
+  #合併所有window，並且加上(t-N)
+  for(i in 1:N){
+    maintable_eachwindow <- maintable[window_N==i,2:length(maintable),]
+    oldname <- c(names(maintable_eachwindow[,2:length(maintable_eachwindow),]))
+    period <- paste0("(t-",i,")")
+    newname <- paste(names(maintable_eachwindow[,2:length(maintable_eachwindow),]),period)
+    setnames(maintable_eachwindow,oldname,newname)
+    CCSWide_window[[i]] <- as.data.table(maintable_eachwindow)
+  }
+
+  #加入label並以label排序
+  series <- Reduce(merge,CCSWide_window)
+  series <- merge(series, unique(personal[,c("ID","label")]),by="ID",all.x=T)
+  class(series)
+  series <- as.data.frame(series)
+  series <- series[,order(colnames(series),decreasing=TRUE),]#series??data.frame?A?~?|?O?諸
+  setDT(series)
+  series <- cbind(series[,c("label","ID")],series[,-c("label","ID")])
+  series <- series[order(label)]
+  #讓放進去的個案最後能被整除
+
+  ##Split dataset into training and testing sets: 分層抽樣
+  ##取train
+  #label = T
+  dices_T  <- sample(1:nrow(series[label==1,,]))
+  folds_T <- cut(dices_T, breaks = 5, label = FALSE)#train test 8:2
+  dices_F  <- sample(1:nrow(series[label==0,,]))
+  folds_F <- cut(dices_F, breaks = 5, label = FALSE)#train test 8:2
+  test_indices_T <- which(folds_T == 1 , arr.ind = TRUE)
+  test_T <- series[label==1,,][test_indices_T,,]
+  test_indices_F <- which(folds_F == 1 , arr.ind = TRUE)
+  test_F <- series[label==0,,][test_indices_F,,]
+  train_T <- series[label==1,,][-test_indices_T,,]
+  train_F <- series[label==0,,][-test_indices_F,,]
+  train <- rbind(train_T,train_F)
+  test <- rbind(test_T,test_F)
+
+  batch_size = 100
+  test_sam <- sample(1:nrow(test))[1:(nrow(test)-nrow(test) %% batch_size)]
+  test <- test[test_sam,,]
+  y_test = array(unlist(test[, 1]))
+  x_test = test[, 3:ncol(test)]
+  x_test <- array(unlist(x_test),dim=c(nrow(test),N,(ncol(test)-2)/N))
+  ###Modeling
+  X_shape2 = N
+  X_shape3 = (ncol(train)-2)/N
+
+  # Epoch = 10
+  # batch_size = 100   # must be a common factor of both the train and test samples
+  # units = 16   # can adjust this, in model tuninig phase
+  loss= 'binary crossentropy' #
+  optimizer ="adam"
+  metrics ="accuracy"
+
+  #Initialize model
+  model <- keras_model_sequential()
+  model %>%
+    layer_lstm(units, batch_input_shape = c(batch_size, X_shape2, X_shape3),return_sequences =FALSE, stateful= TRUE,
+               kernel_regularizer = regularizer_l2(0.001))%>%
+    #layer_dropout(rate=0.2) %>%
+    #layer_lstm(units, return_sequences = FALSE,stateful= TRUE)%>% #statereful?e?@??batchsize???ǦC?O?ЬO?_?Ǩ??U?@??batchsize
+    # layer_dropout(rate=0.2) %>%
+    layer_dense(units = 16,activation = 'relu') %>%
+    layer_dense(units = 1,activation = 'sigmoid')
+
+  # Optimizer
+  model %>% compile(
+    loss = 'binary_crossentropy',
+    optimizer = 'adam',
+    metrics= tf$keras$metrics$AUC())
+  #metrics='acc'
+
+  ##train the model
+  k <- 5
+  set.seed(2)
+  sam_T <- sample(1:nrow(train[label==1]))
+  train_T <- train[label==1,,][sam_T[1:nrow(train[label==1])-nrow(train[label==1])%%5]]
+  dices_T  <- sample(1:nrow(train_T[label==1]))
+  folds_T <- cut(dices_T, breaks = 5, label = FALSE)#train test 8:2
+  sam_F <- sample(1:nrow(train[label==0]))
+  train_F <- train[label==0,,][sam_F[1:(nrow(train[label==0])-nrow(train[label==0])%%5)]]
+  dices_F  <- sample(1:nrow(train_F[label==0,,]))
+  folds_F <- cut(dices_F, breaks = 5, label = FALSE)#train test 8:2
+  folds <- c(folds_T,folds_F)
+  newtrain <- rbind(train_T,train_F)
+
+  y_newtrain = array(unlist(newtrain[, 1]))
+  x_newtrain = newtrain[, 3:ncol(newtrain)]
+  x_newtrain <- array(unlist(x_newtrain),dim=c(nrow(newtrain),N,(ncol(newtrain)-2)/N))#unlist?Nx_train?ܦ??V?q?A?A??array?ܦ??}?C?A?]??array???u?????J?V?q
+
+  all_auc_histories <- NULL
+  for(i in 1:k){
+    val_indices <- which(folds == i , arr.ind = TRUE)
+    val_sam <- sample(1:length(val_indices))[1:(length(val_indices)-length(val_indices)%%batch_size)]
+    val_data <- x_newtrain[val_indices,,][val_sam,,]
+    val_targets <- y_newtrain[val_indices][val_sam]
+    partial_indices <- which(folds != i , arr.ind = TRUE)
+    pattial_train_sam <- sample(1:length(partial_indices))[1:(length(partial_indices)-length(partial_indices)%%batch_size)]
+    partial_train_data <- x_newtrain[partial_indices,,][pattial_train_sam,,]
+    partial_train_targets <- y_newtrain[partial_indices][pattial_train_sam]
+
+    history <- model %>% fit(
+      x = partial_train_data,
+      y = partial_train_targets,
+      batch_size = batch_size,
+      epoch=Epoch,
+      verbose = 1,
+      shuffle = FALSE,
+      validation_data = list(val_data,val_targets)
+    )
+    auc_history <- history$metrics[[4]]
+    all_auc_histories <- rbind(all_auc_histories, auc_history)
+  }
+  ##tag 0307
+  average_auc_history <- data.frame(
+    epoch = seq(1:ncol(all_auc_histories)),
+    validation_auc = apply(all_auc_histories, 2, mean)
+  )
+  setDT(average_auc_history)
+  epoch = average_auc_history[validation_auc==max(average_auc_history$validation_auc),epoch]
+  model %>% fit(
+    x = partial_train_data,
+    y = partial_train_targets,
+    batch_size = batch_size,
+    epoch=epoch ,
+    verbose = 1,
+    shuffle = FALSE
+    #validation_data = list(val_data,val_targets)
+  )
+  auc <- model %>% evaluate(x_test, y_test, batch_size = batch_size)
+  return(list(model=model,x_test=x_test,y_test=y_test,evaluation=auc))
+}
---
+++ R/selectFeature.R
@@ -0,0 +1,73 @@
+selectFeature <- function(DataFile,ID,ICD,date,BIRTHDAY=NULL,label,casecount_limit,type="single",isDescription=F,predictwindow,window=NULL,N=NULL,icdcount){
+  if(is.null(N)){N <- 1}
+  DataFile <- as.data.table(DataFile)
+  if(is.null(BIRTHDAY)){
+    dataCol <- c(deparse(substitute(ID)),deparse(substitute(ICD)),deparse(substitute(date)),deparse(substitute(label)))
+  }else{
+    dataCol <- c(deparse(substitute(ID)),deparse(substitute(ICD)),deparse(substitute(date)),deparse(substitute(label)),deparse(substitute(BIRTHDAY)))
+  }
+  DataFile <- DataFile[,dataCol,with = FALSE]
+  if(is.null(BIRTHDAY)){
+    cutdata <- cutWindow(DataFile, ID, ICD, date, predictwindow=predictwindow , window=window , N=N , icdcount=icdcount)
+  }else{
+    cutdata <- cutWindow(DataFile, ID, ICD, date, BIRTHDAY, predictwindow=predictwindow , window=window , N=N , icdcount=icdcount)
+  }
+
+  COXccs_summary <- list()
+  for(i in 1:N){
+    ##取該window的數據
+    cutdata_bywindow <- cutdata[window_N == i,-c("window_N")]
+    DataFile <- DataFile[, c("predfirstdate", "indexdate") := list(min(date), max(date)), by=ID]
+    ##原始資料取futime數據
+    DataFile_case <- DataFile[,c("ID","futat","futime") := list(ID,label,indexdate-predfirstdate),][,c("ID","futat","futime"),]
+    DataFile_case <- unique(DataFile_case)
+    DataFile_case <- merge(DataFile_case,cutdata_bywindow,by="ID",all.x=T)
+    ##決定以mul/single進行特徵選取
+    if(type=="mul"){
+      #string <- paste(colnames(temp)[4:(ncol(CCSWide)+2)], collapse = "`+`")########從這
+      string <- paste(colnames(DataFile_case)[4:(ncol(DataFile_case))], collapse = "`+`")##因為變數太多會跑不動，所以先到50
+      string2 <- paste0("`", string, "`")
+      f1 <- as.formula(paste("Surv(futime, futat) ~ ", string2))###從這0715 a.m.12:27
+      ova.fit <- coxph(f1, data = DataFile_case)#Error: no function to return from, jumping to top level
+      ova.fit
+      COXccs <- summary(ova.fit)
+      COXccs <- as.data.table(COXccs$coefficients, COXccs$conf.int)
+      setDT(COXccs)
+    }else if(type=="single"){
+      ###(單)
+      COXccs <- data.table()
+      for(j in 4:ncol(DataFile_case)){
+        string2 <- paste0("`", colnames(DataFile_case)[j], "`")
+        f1 <- as.formula(paste("Surv(futime, futat) ~ ", string2))
+        ova.fit_sg <- coxph(f1,data = DataFile_case)
+        COXccs_sgtemp <- summary(ova.fit_sg)
+        COXccs <- rbind(COXccs,as.data.table(COXccs_sgtemp$coefficients, COXccs_sgtemp$conf.int))
+        setDT(COXccs)
+      }
+    }
+    ##計算casecount數量
+    setnames(COXccs,"rn","CCS_CATEGORY")
+    CCSWide_num <- setDT(cutdata_bywindow)[,lapply(cutdata_bywindow[,2:ncol(cutdata_bywindow)],sum),]
+    #CCSWide_num <- gather(CCSWide_num,key=rn,value=count)
+    CCSWide_num <- melt(setDT(CCSWide_num), na.rm = TRUE)[,.(CCS_CATEGORY=variable,count=value)]
+    CCSWide_num$CCS_CATEGORY <- paste0("`",CCSWide_num$CCS_CATEGORY,"`")
+    COXccs <- merge(COXccs,CCSWide_num,by="CCS_CATEGORY",all.x=T)
+    ##決定是否增加文字說明
+    if(isDescription==T){
+      COXccs_des <- unique(ccsDxICD10[,c("CCS_CATEGORY","CCS_CATEGORY_DESCRIPTION","CCS_LVL_2_LABEL","CCS_LVL_1_LABEL"),])
+      COXccs_des <- rename(COXccs_des,"CCS_CATEGORY" = "CCS_CATEGORY")
+      COXccs_des$CCS_CATEGORY <- paste0("`",COXccs_des$CCS_CATEGORY,"`")
+      COXccs <- merge(COXccs,COXccs_des,by="CCS_CATEGORY",all.x=T)
+      COXccs <- COXccs[, c("CCS_CATEGORY","exp(coef)","Pr(>|z|)","count","CCS_CATEGORY_DESCRIPTION","CCS_LVL_2_LABEL","CCS_LVL_1_LABEL")]
+    }else{
+      COXccs <- COXccs[, c("CCS_CATEGORY","exp(coef)","Pr(>|z|)","count")]
+    }
+
+    COXccs <- COXccs[`Pr(>|z|)` < 0.05 & "case count" > casecount_limit , c("selected") := TRUE]
+    COXccs[is.na(COXccs$selected),"selected"] <- FALSE
+    setnames(COXccs,c("exp(coef)","count"),c("HR","case count"))
+    COXccs_summary[[i]] <- COXccs
+
+  }
+  return(COXccs_summary)
+}
